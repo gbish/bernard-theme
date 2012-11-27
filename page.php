34011c8bca2cb668039210f8d2cb85edd09edc4e
Gareth Bishop <hebgb@blueyonder.co.uk>
2012-11-27 00:42:32 +0000

    Initial commit

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div class="content">
+			<div class="single-page">
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+			</div>			
+			<div class="right_bar">
+				<?php get_sidebar(); ?>
+			</div>
+		</div><!-- #content -->
+
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div class="content">
+			<div class="single-post">
+				
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'single' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+				
+			</div>
+
+			<div class="right_bar">
+				<?php get_sidebar(); ?>
+			</div>	
+
+		</div><!-- #content -->
+
+<?php get_footer(); ?>
